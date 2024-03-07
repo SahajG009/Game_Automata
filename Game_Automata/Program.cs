360401b5195e28cb77c7d57c02f047735f7f02c4
Sahaj <sahajguptarocks@gmail.com>
2024-03-07 21:31:50 +0530

    from background to focus

--- Game_Automata/Program.cs
+++ Game_Automata/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,31 @@ namespace Game_Automata
 {
     class Program
     {
+        // import the function of your class
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+        
         static void Main(string[] args)
         {
+            Process[] ps = Process.GetProcessesByName("hl2");
+            //Console.WriteLine(ps.Length);
+            //foreach (var p in ps)
+            //{
+            //   Console.WriteLine(p);
+            //}
+            Process TF2Process = ps.FirstOrDefault();
+            Console.WriteLine(TF2Process);
+
+            if (TF2Process != null)
+            {
+                Console.WriteLine("Bringing TF2 on focus");
+                IntPtr h = TF2Process.MainWindowHandle;
+                SetForegroundWindow(h);
+            }
+
+
+            Console.WriteLine("Press a key to exit.....");
+            Console.ReadKey();
         }
     }
 }
